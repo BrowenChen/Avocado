d274b102be25c1601bef6bb36f41e7eaea4e9145
Browenchen <owenchen@berkeley.edu>
2014-09-09 10:32:35 -0700

    switched to nodeJs backend, flask was screwing up somewhere

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+// Declare app level module which depends on filters, and services
+angular.module('Sheetify', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home/empty.html', 
+        controller: 'HomeController'})
+
+      .when('/register', {
+		templateUrl: 'views/home/register.html',
+		controller: 'HomeController'
+      })
+      .when('/interface', {
+        templateUrl: 'views/home/home.html',
+        controller: 'HomeController'
+      })
+      .when('/review', {
+        templateUrl: 'views/home/review.html',
+        controller: 'HomeController'
+      })
+      .otherwise({redirectTo: '/'});
+  }]);
